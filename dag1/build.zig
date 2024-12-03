a37b486688ffeb92ac355a4d67ae8053c7ea6893
Baldur Tveten <baldur@Baldurs-MacBook-Pro.local>
2024-12-03 09:18:26 +0100

    fixed exe files

--- dag1/build.zig
+++ dag1/build.zig
@@ -31,7 +31,7 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "dag1",
-        .root_source_file = b.path("src/main.zig"),
+        .root_source_file = b.path("src/del_en.zig"),
         .target = target,
         .optimize = optimize,
     });
